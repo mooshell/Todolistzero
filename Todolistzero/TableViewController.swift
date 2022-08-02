8b481d1849d92ae34668db09532899708f600ed7
Michelle Elias Flores <michelleeliasflores@gmail.com>
2022-08-02 13:22:49 -0400

    Initial Commit

---
+++ Todolistzero/TableViewController.swift
@@ -0,0 +1,75 @@
+//
+//  TableViewController.swift
+//  Todolistzero
+//
+//  Created by Michelle Elias Flores on 8/2/22.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    var toDos : [ToDo] = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        toDos = createToDo()
+    }
+
+    func createToDo() -> [ToDo] {
+        
+        let swift = ToDo()
+        swift.name = "Learn Swift"
+        swift.important = true
+        
+        let dog = ToDo()
+        dog.name = "Walk the dog"
+        
+        return [swift,dog]
+    }
+    
+    
+    
+    
+    
+    
+    
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return toDos.count
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    
+
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+   
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Todolistzero/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  Todolistzero
+//
+//  Created by Michelle Elias Flores on 8/2/22.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+    
+}
